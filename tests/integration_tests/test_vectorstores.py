7b2f5f57b26e829982859c2cf0815e7bd945bb98
hsm207 <hsm207@users.noreply.github.com>
2023-12-20 23:01:50 +0000

    reformat code

--- tests/integration_tests/test_vectorstores.py
+++ tests/integration_tests/test_vectorstores.py
@@ -5,18 +5,21 @@ import uuid
 from typing import Generator, Union
 
 import pytest
-from langchain_core.documents import Document
 import requests
-
 from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+
 from langchain_weaviate.vectorstores import WeaviateVectorStore
+
 from .fake_embeddings import FakeEmbeddings
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 def setup_module(module):
-    if 'OPENAI_API_KEY' not in os.environ:
+    if "OPENAI_API_KEY" not in os.environ:
         raise EnvironmentError("OPENAI_API_KEY must be defined before running tests")
+
 
 def is_ready(url):
     try:
@@ -25,6 +28,7 @@ def is_ready(url):
             return True
     except requests.exceptions.ConnectionError:
         return False
+
 
 @pytest.fixture(scope="function")
 def weaviate_url(docker_ip, docker_services):
@@ -37,208 +41,215 @@ def weaviate_url(docker_ip, docker_services):
     )
 
     yield url
-    
+
+
 @pytest.fixture
 def embedding_openai():
     yield OpenAIEmbeddings()
 
-def test_similarity_search_without_metadata(
-        weaviate_url: str, embedding_openai: OpenAIEmbeddings
-    ) -> None:
-        """Test end to end construction and search without metadata."""
-        texts = ["foo", "bar", "baz"]
-        docsearch = WeaviateVectorStore.from_texts(
-            texts,
-            embedding_openai,
-            weaviate_url=weaviate_url,
-        )
 
-        output = docsearch.similarity_search("foo", k=1)
-        assert output == [Document(page_content="foo")]
+def test_similarity_search_without_metadata(
+    weaviate_url: str, embedding_openai: OpenAIEmbeddings
+) -> None:
+    """Test end to end construction and search without metadata."""
+    texts = ["foo", "bar", "baz"]
+    docsearch = WeaviateVectorStore.from_texts(
+        texts,
+        embedding_openai,
+        weaviate_url=weaviate_url,
+    )
+
+    output = docsearch.similarity_search("foo", k=1)
+    assert output == [Document(page_content="foo")]
+
 
 def test_similarity_search_with_metadata(
-        weaviate_url: str, embedding_openai: OpenAIEmbeddings
-    ) -> None:
-        """Test end to end construction and search with metadata."""
-        texts = ["foo", "bar", "baz"]
-        metadatas = [{"page": i} for i in range(len(texts))]
-        docsearch = WeaviateVectorStore.from_texts(
-            texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
-        )
-        output = docsearch.similarity_search("foo", k=1)
-        assert output == [Document(page_content="foo", metadata={"page": 0})]
+    weaviate_url: str, embedding_openai: OpenAIEmbeddings
+) -> None:
+    """Test end to end construction and search with metadata."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": i} for i in range(len(texts))]
+    docsearch = WeaviateVectorStore.from_texts(
+        texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
+    )
+    output = docsearch.similarity_search("foo", k=1)
+    assert output == [Document(page_content="foo", metadata={"page": 0})]
+
 
 def test_similarity_search_with_metadata_and_filter(
-        weaviate_url: str, embedding_openai: OpenAIEmbeddings
-    ) -> None:
-        """Test end to end construction and search with metadata."""
-        texts = ["foo", "bar", "baz"]
-        metadatas = [{"page": i} for i in range(len(texts))]
-        docsearch = WeaviateVectorStore.from_texts(
-            texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
-        )
-        output = docsearch.similarity_search(
-            "foo",
-            k=2,
-            where_filter={"path": ["page"], "operator": "Equal", "valueNumber": 0},
-        )
-        assert output == [Document(page_content="foo", metadata={"page": 0})]
+    weaviate_url: str, embedding_openai: OpenAIEmbeddings
+) -> None:
+    """Test end to end construction and search with metadata."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": i} for i in range(len(texts))]
+    docsearch = WeaviateVectorStore.from_texts(
+        texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
+    )
+    output = docsearch.similarity_search(
+        "foo",
+        k=2,
+        where_filter={"path": ["page"], "operator": "Equal", "valueNumber": 0},
+    )
+    assert output == [Document(page_content="foo", metadata={"page": 0})]
+
 
 def test_similarity_search_with_metadata_and_additional(
-        weaviate_url: str, embedding_openai: OpenAIEmbeddings
-    ) -> None:
-        """Test end to end construction and search with metadata and additional."""
-        texts = ["foo", "bar", "baz"]
-        metadatas = [{"page": i} for i in range(len(texts))]
-        docsearch = WeaviateVectorStore.from_texts(
-            texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
+    weaviate_url: str, embedding_openai: OpenAIEmbeddings
+) -> None:
+    """Test end to end construction and search with metadata and additional."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": i} for i in range(len(texts))]
+    docsearch = WeaviateVectorStore.from_texts(
+        texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
+    )
+    output = docsearch.similarity_search(
+        "foo",
+        k=1,
+        additional=["certainty"],
+    )
+    assert output == [
+        Document(
+            page_content="foo",
+            metadata={"page": 0, "_additional": {"certainty": 1.0000003576278687}},
         )
-        output = docsearch.similarity_search(
-            "foo",
-            k=1,
-            additional=["certainty"],
-        )
-        assert output == [
-            Document(
-                page_content="foo",
-                metadata={"page": 0, "_additional": {"certainty": 1.0000003576278687}},
-            )
-        ]
+    ]
+
 
 def test_similarity_search_with_uuids(
-        weaviate_url: str, embedding_openai: OpenAIEmbeddings
-    ) -> None:
-        """Test end to end construction and search with uuids."""
-        texts = ["foo", "bar", "baz"]
-        # Weaviate replaces the object if the UUID already exists
-        uuids = [uuid.uuid5(uuid.NAMESPACE_DNS, "same-name") for text in texts]
+    weaviate_url: str, embedding_openai: OpenAIEmbeddings
+) -> None:
+    """Test end to end construction and search with uuids."""
+    texts = ["foo", "bar", "baz"]
+    # Weaviate replaces the object if the UUID already exists
+    uuids = [uuid.uuid5(uuid.NAMESPACE_DNS, "same-name") for text in texts]
 
-        metadatas = [{"page": i} for i in range(len(texts))]
-        docsearch = WeaviateVectorStore.from_texts(
-            texts,
-            embedding_openai,
-            metadatas=metadatas,
-            weaviate_url=weaviate_url,
-            uuids=uuids,
-        )
-        output = docsearch.similarity_search("foo", k=2)
-        assert len(output) == 1
+    metadatas = [{"page": i} for i in range(len(texts))]
+    docsearch = WeaviateVectorStore.from_texts(
+        texts,
+        embedding_openai,
+        metadatas=metadatas,
+        weaviate_url=weaviate_url,
+        uuids=uuids,
+    )
+    output = docsearch.similarity_search("foo", k=2)
+    assert len(output) == 1
+
 
 def test_max_marginal_relevance_search(
-        weaviate_url: str, embedding_openai: OpenAIEmbeddings
-    ) -> None:
-        """Test end to end construction and MRR search."""
-        texts = ["foo", "bar", "baz"]
-        metadatas = [{"page": i} for i in range(len(texts))]
+    weaviate_url: str, embedding_openai: OpenAIEmbeddings
+) -> None:
+    """Test end to end construction and MRR search."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": i} for i in range(len(texts))]
 
-        docsearch = WeaviateVectorStore.from_texts(
-            texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
-        )
-        # if lambda=1 the algorithm should be equivalent to standard ranking
-        standard_ranking = docsearch.similarity_search("foo", k=2)
-        output = docsearch.max_marginal_relevance_search(
-            "foo", k=2, fetch_k=3, lambda_mult=1.0
-        )
-        assert output == standard_ranking
+    docsearch = WeaviateVectorStore.from_texts(
+        texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
+    )
+    # if lambda=1 the algorithm should be equivalent to standard ranking
+    standard_ranking = docsearch.similarity_search("foo", k=2)
+    output = docsearch.max_marginal_relevance_search(
+        "foo", k=2, fetch_k=3, lambda_mult=1.0
+    )
+    assert output == standard_ranking
 
-        # if lambda=0 the algorithm should favour maximal diversity
-        output = docsearch.max_marginal_relevance_search(
-            "foo", k=2, fetch_k=3, lambda_mult=0.0
-        )
-        assert output == [
-            Document(page_content="foo", metadata={"page": 0}),
-            Document(page_content="bar", metadata={"page": 1}),
-        ]
+    # if lambda=0 the algorithm should favour maximal diversity
+    output = docsearch.max_marginal_relevance_search(
+        "foo", k=2, fetch_k=3, lambda_mult=0.0
+    )
+    assert output == [
+        Document(page_content="foo", metadata={"page": 0}),
+        Document(page_content="bar", metadata={"page": 1}),
+    ]
+
 
 def test_max_marginal_relevance_search_by_vector(
-        weaviate_url: str, embedding_openai: OpenAIEmbeddings
-    ) -> None:
-        """Test end to end construction and MRR search by vector."""
-        texts = ["foo", "bar", "baz"]
-        metadatas = [{"page": i} for i in range(len(texts))]
+    weaviate_url: str, embedding_openai: OpenAIEmbeddings
+) -> None:
+    """Test end to end construction and MRR search by vector."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": i} for i in range(len(texts))]
 
-        docsearch = WeaviateVectorStore.from_texts(
-            texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
-        )
-        foo_embedding = embedding_openai.embed_query("foo")
+    docsearch = WeaviateVectorStore.from_texts(
+        texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
+    )
+    foo_embedding = embedding_openai.embed_query("foo")
 
-        # if lambda=1 the algorithm should be equivalent to standard ranking
-        standard_ranking = docsearch.similarity_search("foo", k=2)
-        output = docsearch.max_marginal_relevance_search_by_vector(
-            foo_embedding, k=2, fetch_k=3, lambda_mult=1.0
-        )
-        assert output == standard_ranking
+    # if lambda=1 the algorithm should be equivalent to standard ranking
+    standard_ranking = docsearch.similarity_search("foo", k=2)
+    output = docsearch.max_marginal_relevance_search_by_vector(
+        foo_embedding, k=2, fetch_k=3, lambda_mult=1.0
+    )
+    assert output == standard_ranking
 
-        # if lambda=0 the algorithm should favour maximal diversity
-        output = docsearch.max_marginal_relevance_search_by_vector(
-            foo_embedding, k=2, fetch_k=3, lambda_mult=0.0
-        )
-        assert output == [
-            Document(page_content="foo", metadata={"page": 0}),
-            Document(page_content="bar", metadata={"page": 1}),
-        ]
+    # if lambda=0 the algorithm should favour maximal diversity
+    output = docsearch.max_marginal_relevance_search_by_vector(
+        foo_embedding, k=2, fetch_k=3, lambda_mult=0.0
+    )
+    assert output == [
+        Document(page_content="foo", metadata={"page": 0}),
+        Document(page_content="bar", metadata={"page": 1}),
+    ]
+
 
 def test_max_marginal_relevance_search_with_filter(
-        weaviate_url: str, embedding_openai: OpenAIEmbeddings
-    ) -> None:
-        """Test end to end construction and MRR search."""
-        texts = ["foo", "bar", "baz"]
-        metadatas = [{"page": i} for i in range(len(texts))]
+    weaviate_url: str, embedding_openai: OpenAIEmbeddings
+) -> None:
+    """Test end to end construction and MRR search."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": i} for i in range(len(texts))]
 
-        docsearch = WeaviateVectorStore.from_texts(
-            texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
-        )
-        where_filter = {"path": ["page"], "operator": "Equal", "valueNumber": 0}
-        # if lambda=1 the algorithm should be equivalent to standard ranking
-        standard_ranking = docsearch.similarity_search(
-            "foo", k=2, where_filter=where_filter
-        )
-        output = docsearch.max_marginal_relevance_search(
-            "foo", k=2, fetch_k=3, lambda_mult=1.0, where_filter=where_filter
-        )
-        assert output == standard_ranking
+    docsearch = WeaviateVectorStore.from_texts(
+        texts, embedding_openai, metadatas=metadatas, weaviate_url=weaviate_url
+    )
+    where_filter = {"path": ["page"], "operator": "Equal", "valueNumber": 0}
+    # if lambda=1 the algorithm should be equivalent to standard ranking
+    standard_ranking = docsearch.similarity_search(
+        "foo", k=2, where_filter=where_filter
+    )
+    output = docsearch.max_marginal_relevance_search(
+        "foo", k=2, fetch_k=3, lambda_mult=1.0, where_filter=where_filter
+    )
+    assert output == standard_ranking
 
-        # if lambda=0 the algorithm should favour maximal diversity
-        output = docsearch.max_marginal_relevance_search(
-            "foo", k=2, fetch_k=3, lambda_mult=0.0, where_filter=where_filter
-        )
-        assert output == [
-            Document(page_content="foo", metadata={"page": 0}),
-        ]
+    # if lambda=0 the algorithm should favour maximal diversity
+    output = docsearch.max_marginal_relevance_search(
+        "foo", k=2, fetch_k=3, lambda_mult=0.0, where_filter=where_filter
+    )
+    assert output == [
+        Document(page_content="foo", metadata={"page": 0}),
+    ]
+
 
 def test_add_texts_with_given_embedding(weaviate_url: str) -> None:
-        texts = ["foo", "bar", "baz"]
-        embedding = FakeEmbeddings()
+    texts = ["foo", "bar", "baz"]
+    embedding = FakeEmbeddings()
 
-        docsearch = WeaviateVectorStore.from_texts(
-            texts, embedding=embedding, weaviate_url=weaviate_url
-        )
+    docsearch = WeaviateVectorStore.from_texts(
+        texts, embedding=embedding, weaviate_url=weaviate_url
+    )
 
-        docsearch.add_texts(["foo"])
-        output = docsearch.similarity_search_by_vector(
-            embedding.embed_query("foo"), k=2
-        )
-        assert output == [
-            Document(page_content="foo"),
-            Document(page_content="foo"),
-        ]
+    docsearch.add_texts(["foo"])
+    output = docsearch.similarity_search_by_vector(embedding.embed_query("foo"), k=2)
+    assert output == [
+        Document(page_content="foo"),
+        Document(page_content="foo"),
+    ]
+
 
 def test_add_texts_with_given_uuids(weaviate_url: str) -> None:
-        texts = ["foo", "bar", "baz"]
-        embedding = FakeEmbeddings()
-        uuids = [uuid.uuid5(uuid.NAMESPACE_DNS, text) for text in texts]
+    texts = ["foo", "bar", "baz"]
+    embedding = FakeEmbeddings()
+    uuids = [uuid.uuid5(uuid.NAMESPACE_DNS, text) for text in texts]
 
-        docsearch = WeaviateVectorStore.from_texts(
-            texts,
-            embedding=embedding,
-            weaviate_url=weaviate_url,
-            uuids=uuids,
-        )
+    docsearch = WeaviateVectorStore.from_texts(
+        texts,
+        embedding=embedding,
+        weaviate_url=weaviate_url,
+        uuids=uuids,
+    )
 
-        # Weaviate replaces the object if the UUID already exists
-        docsearch.add_texts(["foo"], uuids=[uuids[0]])
-        output = docsearch.similarity_search_by_vector(
-            embedding.embed_query("foo"), k=2
-        )
-        assert output[0] == Document(page_content="foo")
-        assert output[1] != Document(page_content="foo")
+    # Weaviate replaces the object if the UUID already exists
+    docsearch.add_texts(["foo"], uuids=[uuids[0]])
+    output = docsearch.similarity_search_by_vector(embedding.embed_query("foo"), k=2)
+    assert output[0] == Document(page_content="foo")
+    assert output[1] != Document(page_content="foo")
