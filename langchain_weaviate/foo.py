81cc9d0713e7ce825c64a68122015ea3e80d52a7
hsm207 <hsm207@users.noreply.github.com>
2024-02-13 22:41:24 +0000

    another codeql test

--- langchain_weaviate/foo.py
+++ langchain_weaviate/foo.py
@@ -2,3 +2,30 @@ def area(r):
     # if DEBUG:
     #   print("Computing area of %r" % r)
     return r.length * r.width
+
+
+CONFIG_FILE = "foo.txt"
+
+
+def process_request(request):
+    password = request.GET["password"]
+
+    # BAD: Inbound authentication made by comparison to string literal
+    if password == "myPa55word":
+        redirect("login")
+
+    hashed_password = load_from_config("hashed_password", CONFIG_FILE)
+    salt = load_from_config("salt", CONFIG_FILE)
+
+    print("Hashed password: %r" % hashed_password)
+    print("Salt: %r" % salt)
+
+
+def redirect(url):
+    # TODO: Implement redirect logic
+    pass
+
+
+def load_from_config(key, config_file):
+    # TODO: Implement loading from config logic
+    pass
